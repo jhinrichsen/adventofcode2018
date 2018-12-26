962b97acc6fb585e4f3db66d2ee930df8547cb5d
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2018-12-26 14:45:29 +0100

    feat: day 1 part #2

--- day1/main_test.go
+++ day1/main_test.go
@@ -1,19 +1,63 @@
-package main
+package day1
 
 import (
-	"fmt"
+	"bufio"
+	"log"
 	"os"
+	"strconv"
 	"testing"
 )
 
-func TestDay1(t *testing.T) {
+func frequencies() ([]int, error) {
 	f, err := os.Open("testdata/input")
 	if err != nil {
-		t.Error(err)
+		return nil, err
 	}
-	sum, err := day1_1(f)
+	defer f.Close()
+	sc := bufio.NewScanner(f)
+	var fs []int
+	for sc.Scan() {
+		line := sc.Text()
+		n, err := strconv.Atoi(line)
+		if err != nil {
+			return nil, err
+		}
+		fs = append(fs, n)
+	}
+	return fs, nil
+}
+
+func TestPart1(t *testing.T) {
+	fs, err := frequencies()
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Printf("%d\n", sum)
+	sum := 0
+	for _, n := range fs {
+		sum += n
+	}
+	log.Printf("Sum: %d\n", sum)
+}
+
+func TestPart2(t *testing.T) {
+	fs, err := frequencies()
+	if err != nil {
+		t.Fatal(err)
+	}
+	twice := make(map[int]bool)
+	sum := 0
+	i := 0
+	for {
+		sum += fs[i]
+		if twice[sum] {
+			break
+		}
+		twice[sum] = true
+
+		i++
+		if i == len(fs) {
+			i = 0
+		}
+	}
+	log.Printf("Twice: %d\n", sum)
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,6 @@
 module gitlab.com/jhinrichsen/adventofcode-2018
+
+require (
+	github.com/google/pprof v0.0.0-20181206194817-3ea8567a2e57 // indirect
+	golang.org/x/arch v0.0.0-20181203225421-5a4828bb7045 // indirect
+)
