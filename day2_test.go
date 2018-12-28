cb71d1f6fecc3eddf54d4a9a2911465ed4455b8d
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2018-12-28 17:35:09 +0100

    feat: day 2 part 2

--- day2_test.go
+++ day2_test.go
@@ -33,7 +33,7 @@ func day2(boxIDs []string) int {
 	return appear(boxIDs, 2) * appear(boxIDs, 3)
 }
 
-func TestSample(t *testing.T) {
+func TestSamplePart1(t *testing.T) {
 	ids := []string{
 		"abcdef",
 		"bababc",
@@ -63,6 +63,71 @@ func TestDay2(t *testing.T) {
 	}
 	want := 5434
 	got := day2(IDs)
+	if want != got {
+		t.Fatalf("want %v but got %v\n", want, got)
+	}
+}
+
+func sameRunes(s1, s2 string) string {
+	var bs []byte
+	for i := 0; i < len(s1); i++ {
+		if s1[i] == s2[i] {
+			bs = append(bs, s1[i])
+		}
+	}
+	return string(bs)
+}
+
+func TestSameRunes(t *testing.T) {
+	want := "fgij"
+	got := sameRunes("fghij", "fguij")
+	if want != got {
+		t.Fatalf("want %v but got %v\n", want, got)
+	}
+}
+
+func day2Part2(IDs []string) string {
+	for i := range IDs {
+		for j := i; j < len(IDs); j++ {
+			s := sameRunes(IDs[i], IDs[j])
+			if len(IDs[i])-1 == len(s) {
+				return s
+			}
+		}
+	}
+	return ""
+}
+
+func TestSamplePart2(t *testing.T) {
+	ids := []string{
+		"abcde",
+		"fghij",
+		"klmno",
+		"pqrst",
+		"fguij",
+		"axcye",
+		"wvxyz",
+	}
+	want := "fgij"
+	got := day2Part2(ids)
+	if want != got {
+		t.Fatalf("want %v but got %v\n", want, got)
+	}
+}
+
+func TestDay2Part2(t *testing.T) {
+	f, err := os.Open("testdata/day2")
+	if err != nil {
+		t.Fatal(err)
+	}
+	var IDs []string
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		line := sc.Text()
+		IDs = append(IDs, line)
+	}
+	want := "agimdjvlhedpsyoqfzuknpjwt"
+	got := day2Part2(IDs)
 	if want != got {
 		t.Fatalf("want %v but got %v\n", want, got)
 	}
